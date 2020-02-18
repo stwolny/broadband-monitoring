b268fb69bd523c59def1d58c32a9f15a9a874b69
stwolny <stwolny@gmail.com>
2020-02-18 20:15:34 +0000

    updated speedtest data

--- speedtest.py
+++ speedtest.py
@@ -3,7 +3,7 @@ import re
 import subprocess
 import time
 
-response = subprocess.Popen('speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
+response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
 
 ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
 download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
